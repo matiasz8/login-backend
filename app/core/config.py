0772f63ca10ac931e91cd3ecba16f06393017d33
Nahuel Quiroga <nahuelq.78@gmail.com>
2024-10-23 11:22:41 -0300

    fix(config): fixing env vars!

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,12 @@
-import os
 from pydantic import Field
+from pydantic_settings import BaseSettings
 
-class Settings:
+
+class Settings(BaseSettings):
     SECRET_KEY: str = Field(alias="SECRET_KEY", default="mysecretkey")
     title: str = Field(alias="APP_TITLE", default="Login's API")
     version: str = Field(alias="APP_VERSION", default="0.0.1")
     stage: str = Field(alias="STAGE", default="local")
+
 
 settings = Settings()
