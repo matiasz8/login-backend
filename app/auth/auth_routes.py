084f6e2374534ee7ffb51cef83b672bde681bba6
Nahuel Quiroga <nahuelq.78@gmail.com>
2024-10-24 13:10:37 -0300

    fix(login): adding hashes

--- app/auth/auth_routes.py
+++ app/auth/auth_routes.py
@@ -1,3 +1,4 @@
+import hashlib
 from fastapi import APIRouter, HTTPException
 
 from app.auth.auth_handler import create_access_token
@@ -5,16 +6,14 @@ from app.schemas.user import User
 
 auth_router = APIRouter()
 
-users_db = {"testuser": {"username": "testuser", "password": "testpass"}}
-
+users_db = [{"email": "test@mail.com", "hashed_password": hashlib.sha256("test".encode('utf-8')).hexdigest()}]
 
 @auth_router.post("/login")
 def login(user: User):
-    if (
-        user.username in users_db
-        and users_db[user.username]["password"] == user.password
-    ):
-        token = create_access_token({"sub": user.username})
-        return {"access_token": token, "token_type": "bearer"}
-
-    raise HTTPException(status_code=401, detail="Invalid username or password")
+    for user_db in users_db:
+        if user_db["email"] == user.email:
+            hashed_password = hashlib.sha256(user.password.encode('utf-8')).hexdigest()
+            if hashed_password == user_db["hashed_password"]:
+                token = create_access_token({"sub": user.email})
+                return {"access_token": token, "token_type": "bearer"}
+    raise HTTPException(status_code=401, detail="Invalid email or password")
--- app/main.py
+++ app/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 
 from app.auth.auth_routes import auth_router
 from app.routes.protected_routes import protected_router
+# add cors middleware
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*",]
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(protected_router)
